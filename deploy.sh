1ef8fe0e0012fe28a2263bdfaf88a21ddfca5554
myliang <liangyuliang0335@gmail.com>
2012-12-24 23:01:07 +0800

    input line-height padding

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
-cp javascripts/application.js ~/rubyworks/zhuata/app/assets/javascripts/better.js
-cp csses/application.css ~/rubyworks/zhuata/app/assets/stylesheets/better.css.scss
+cp doc/javascripts/application.js ~/rubyworks/zhuata/app/assets/javascripts/better.js
+cp doc/csses/application.css ~/rubyworks/zhuata/app/assets/stylesheets/better.css.scss
